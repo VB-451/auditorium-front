657de8cda659754fa7ca0c0ebd3174ebd54ef937
Vlad Burduhosu <114860564+VB-451@users.noreply.github.com>
2024-11-06 09:30:10 +0200

    Minor Changes

--- src/app/components/course-preview/course-preview.tsx
+++ src/app/components/course-preview/course-preview.tsx
@@ -11,7 +11,7 @@ export default function CoursePreview({ data } : {data: CourseData }){
     return (
         <>
             <div className={`w-1/5 max-w-72 min-w-64 h-72 m-3 bg-white border border-gray-400 rounded-xl flex flex-col justify-start items-start`}>
-                <div className={`h-1/2 w-full p-4 rounded-t-xl ${styles.coursePreview}`}>
+                <div className={`h-1/2 w-full p-4 ${styles.coursePreview}`}>
                     <div className={`font-bold text-white text-3xl hover:underline`}>
                         <Link href={`/course/${data.id}`}>{`${data.name}`}</Link>
                     </div>
--- src/app/components/sidebar/sidebar.tsx
+++ src/app/components/sidebar/sidebar.tsx
@@ -1,15 +1,23 @@
+"use client"
+
 import styles from "./styles.module.css"
 import Link from "next/link";
 import Image from "next/image";
+import React from "react";
 
 export default function Sidebar() {
 
+    const [isStudent, setIsStudent] = React.useState(true);
 
     return (
         <aside className={`fixed left-0 top-0 h-full border-r border-gray-500 0 text-black shadow-lg ${styles.aside}`}>
             <div className="p-4 pt-24 h-48 flex flex-col justify-around">
-                <Link href="/student"><Image src="/student.svg" width={30} height={0} alt="Student Icon" /></Link>
-                <Link href="/teacher"><Image src="/teacher.svg" width={30} height={0} alt="Teacher Icon" /></Link>
+                <Link title="See courses you're enrolled to." onClick={()=>{setIsStudent(true)}} href="/student">
+                    <Image src={isStudent ? "/student-active.svg" : "/student.svg"} width={30} height={0} alt="Student Icon" />
+                </Link>
+                <Link title="See courses you manage." onClick={()=>{setIsStudent(false)}} href="/teacher">
+                    <Image src={!isStudent ? "/teacher-active.svg" : "/teacher.svg"} width={30} height={0} alt="Teacher Icon" />
+                </Link>
             </div>
         </aside>
     );
